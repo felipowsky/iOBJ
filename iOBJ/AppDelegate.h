141a13acaec5704106b39284b41b8c7d13002dee
Felipe Augusto <felipowsky@gmail.com>
2012-01-02 20:02:13 -0200

    initial files from ios opengl template

---
+++ iOBJ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOBJ
+//
+//  Created by Felipe Imianowsky on 02/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOBJ/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  iOBJ
+//
+//  Created by Felipe Imianowsky on 02/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
